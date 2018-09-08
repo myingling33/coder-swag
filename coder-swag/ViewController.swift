e20198077cf39e91eddf8d62bcc9f535b19c15c2
Michael David Yingling <michaelyingling@michaels-mbp.gateway.pace.com>
2018-09-08 16:59:58 -0500

    initial commit

---
+++ coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Michael David Yingling on 9/8/18.
+//  Copyright © 2018 Michael David Yingling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
